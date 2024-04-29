5010575aa51d3122ae0e57d486c1c804efe8baf7
Serialbocks <john@serialbocks.com>
2024-04-29 15:34:08 -0400

    Poll for board state

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
+import sys, os, time
 import serial
+
 import dgt_constants
 
-ser = serial.Serial(port='COM7', baudrate=9600, timeout=3)
-print(ser)
-ser.write(bytes([dgt_constants.DGT_SEND_BRD]))
-ser.write(bytes([dgt_constants.DGT_SEND_UPDATE_NICE]))
-s = ser.read(1000)
+ser = None
 
-print(s)
+def main():
+    ser = serial.Serial(port='COM7', baudrate=9600)
+    ser.write(bytes([dgt_constants.DGT_SEND_RESET]))
+    
+    while True:
+        ser.write(bytes([dgt_constants.DGT_SEND_BRD]))
+        time.sleep(0.1)
+        s = b''
+        bytes_read = 0
+        while(ser.in_waiting > 0):
+            c = ser.read(1)
+            s += c
+            bytes_read += 1
+        
+        print('bytes read: ' + str(bytes_read))
+        print(s)
+        time.sleep(0.1)
 
-ser.close()
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        if ser is not None and ser.is_open:
+            ser.close()
+        try:
+            sys.exit(130)
+        except SystemExit:
+            os._exit(130)
