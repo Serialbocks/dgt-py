436229946e9f7b74f10aab30391fb5e33c79cd81
Serialbocks <john@serialbocks.com>
2024-05-06 13:40:12 -0400

    Update install script

--- install-rpi.sh
+++ install-rpi.sh
@@ -1,4 +1,5 @@
 sudo apt-get install -y build-essential zlib1g-dev uuid-dev liblzma-dev lzma-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev libbz2-dev < /dev/null
+sudo apt install -y libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev python3-tk tk-dev
 
 # install python
 new_version="3.10.2"
@@ -10,11 +11,13 @@ wget "${url}"
 echo "Decompressing file"
 tar -Jxf "${file}" < /dev/null
 
+cd Python-${new_version}
 echo "Prepare the source for the installation"
 ./configure --enable-optimizations --prefix=/usr < /dev/null
 make -j$(nproc) < /dev/null
 echo "(Install the new Python version $new_version)"
 sudo make install -j$(nproc)
+sudo ./configure --with-tcltk-includes='-I/usr/include -I/usr/include/tcl' --with-tcltk-libs='-L/usr/lib -ltcl -ltk' --enable-optimizations
 
 echo "Let's cleanup!"
 cd ..
@@ -34,6 +37,18 @@ pip install -r ./requirements.txt
 
 git clone --recurse-submodules https://github.com/SeleniumHQ/selenium.git
 cd selenium/rust
-curl https://sh.rustup.rs -sSf | sh
+curl https://sh.rustup.rs -sSf > install-rust.sh
+chmod +x ./install-rust.sh
+./install-rust.sh -y
 . "$HOME/.cargo/env"
 cargo build
+
+sudo cp target/debug/selenium-manager /usr/bin/selenium-manager
+export SE_MANAGER_PATH=/usr/bin/selenium-manager
+echo "export SE_MANAGER_PATH=/usr/bin/selenium-manager" >> $HOME/.bashrc
+
+echo "Let's cleanup!"
+cd ../..
+rm -rf "Python-$new_version"
+rm -r "${file}"
+rm -rf "selenium"
