df1a99adcb4f3d7ab480a7bb191af3a0391dee4d
Serialbocks <john@serialbocks.com>
2024-05-06 11:20:54 -0400

    Add more to install script

--- install-rpi.sh
+++ install-rpi.sh
@@ -1,5 +1,39 @@
-sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
-libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
-libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev libssl-dev
+sudo apt-get install -y build-essential zlib1g-dev uuid-dev liblzma-dev lzma-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev libbz2-dev < /dev/null
 
+# install python
+new_version="3.10.2"
+file="Python-${new_version}.tar.xz"
+url="https://www.python.org/ftp/python/${new_version}/${file}"
 
+echo "Downloading Python ${new_version}"
+wget "${url}"
+echo "Decompressing file"
+tar -Jxf "${file}" < /dev/null
+
+echo "Prepare the source for the installation"
+./configure --enable-optimizations --prefix=/usr < /dev/null
+make -j$(nproc) < /dev/null
+echo "(Install the new Python version $new_version)"
+sudo make install -j$(nproc)
+
+echo "Let's cleanup!"
+cd ..
+rm -rf "Python-$new_version"
+rm -r "${file}"
+
+python3.10 -m venv venv
+source venv/bin/activate
+
+echo "Let's install PIP"
+sudo apt install -y python3-pip < /dev/null
+
+echo "updating pip..."
+python -m pip install --upgrade pip
+
+pip install -r ./requirements.txt
+
+git clone --recurse-submodules https://github.com/SeleniumHQ/selenium.git
+cd selenium/rust
+curl https://sh.rustup.rs -sSf | sh
+. "$HOME/.cargo/env"
+cargo build
