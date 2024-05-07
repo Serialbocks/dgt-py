1d74672a1ab246064722a96b0712822f954e87e5
Serialbocks <john@serialbocks.com>
2024-05-07 11:29:16 -0400

    sync

--- gui.py
+++ gui.py
@@ -2,46 +2,64 @@ import sys, os, glob, subprocess, time
 import serial
 from PyQt5 import QtWidgets, uic
 
-def serial_ports():
-    """ Lists serial port names
+class Gui:
+    def __init__(self):
+        app = QtWidgets.QApplication(sys.argv)
 
-        :raises EnvironmentError:
-            On unsupported or unknown platforms
-        :returns:
-            A list of the serial ports available on the system
-    """
-    if sys.platform.startswith('win'):
-        ports = ['COM%s' % (i + 1) for i in range(256)]
-    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-        # this excludes your current terminal "/dev/tty"
-        subprocess.Popen(['sh', "./open-bt-connection.sh"])
-        time.sleep(5)
-        ports = ['/dev/rfcomm0']
-    elif sys.platform.startswith('darwin'):
-        ports = glob.glob('/dev/tty.*')
-    else:
-        raise EnvironmentError('Unsupported platform')
+        self.window = uic.loadUi("ui/main.ui")
 
-    result = []
-    for port in ports:
-        try:
-            s = serial.Serial(port)
-            s.close()
-            result.append(port)
-        except (OSError, serial.SerialException):
-            pass
-    return result
+        ports = self.serial_ports()
+        serialPort = self.window.serialPort
+        serialPort.addItems(ports)
+        serialPort.currentIndexChanged.connect(self.serial_index_changed)
+
+        self.window.connectButton.clicked.connect(self.connect_button_pressed)
+
+        self.window.show()
+        app.exec()
+
+    def connect_button_pressed(self):
+        pass
+
+    def serial_index_changed(self, i):
+        print(self.window.serialPort.currentText())
+        if i >= 0:
+            self.window.connectBoard.setEnabled(True)
+        else:
+            self.window.connectBoard.setEnabled(False)
+
+    def serial_ports(self):
+        """ Lists serial port names
+
+            :raises EnvironmentError:
+                On unsupported or unknown platforms
+            :returns:
+                A list of the serial ports available on the system
+        """
+        if sys.platform.startswith('win'):
+            ports = ['COM%s' % (i + 1) for i in range(256)]
+        elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+            # this excludes your current terminal "/dev/tty"
+            subprocess.Popen(['sh', "./open-bt-connection.sh"])
+            time.sleep(5)
+            ports = ['/dev/rfcomm0']
+        elif sys.platform.startswith('darwin'):
+            ports = glob.glob('/dev/tty.*')
+        else:
+            raise EnvironmentError('Unsupported platform')
+
+        result = []
+        for port in ports:
+            try:
+                s = serial.Serial(port)
+                s.close()
+                result.append(port)
+            except (OSError, serial.SerialException):
+                pass
+        return result
 
 def main():
-    app = QtWidgets.QApplication(sys.argv)
-
-    window = uic.loadUi("ui/main.ui")
-
-    ports = serial_ports()
-    window.serialPort.addItems(ports)
-
-    window.show()
-    app.exec()
+    gui = Gui()
 
 if __name__ == '__main__':
     try:
